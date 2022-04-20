a9b0afeadc722199014ff1e0f785f809f4830368
robertomahl <75228333+robertomahl@users.noreply.github.com>
2022-04-20 16:21:54 -0300

    haskell02.hs

--- haskell02.hs
+++ haskell02.hs
@@ -13,10 +13,22 @@ comFebre' t = filter (\ t -> t > 37.8) t
 itemize :: [String] -> [String]
 itemize l = map (\x -> "<li>" ++ x ++ "</li>") l
 
---4 Precisa retornar os raios e não as áreas
---bigCircles :: Float -> [Float] -> [Float]
---bigCircles n r = filter (\x -> x > n) $ map (\y -> pi * y ^ 2) r
+--4
+bigCircles :: Float -> [Float] -> [Float]
+bigCircles n r = filter (\x -> pi * x ^ 2 > n) r
 
 --5
---quarentena :: [(String,Float)] -> [(String,Float)]
---quarentena 
+quarentena :: [(String,Float)] -> [(String,Float)]
+quarentena l = filter (\(_, temperatura) -> eFebre temperatura) l
+
+--6
+idadesEm :: [Int] -> Int -> [Int]
+idadesEm lista ano = map (\nasc -> ano - nasc) lista
+
+--7
+changeNames :: [String] -> [String]
+changeNames nomes = map (\n -> if head n == 'A' then "Super " ++ n else n) nomes
+
+--8
+onlyShorts :: [String] -> [String]
+onlyShorts lista = filter (\p -> length p < 5) lista
